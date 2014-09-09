32be250d5f8f1015becd94dd9c36b95a2544c225
Salman Suhail <salmansqadeer@gmail.com>
2014-09-08 22:42:02 -0400

    Angular hotkeys successfully added.  This is awesome

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@
 var ApplicationConfiguration = (function() {
 	// Init module configuration options
 	var applicationModuleName = 'taskmanager';
-	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils'];
+	var applicationModuleVendorDependencies = ['ngResource', 'ngCookies',  'ngAnimate',  'ngTouch',  'ngSanitize',  'ui.router', 'ui.bootstrap', 'ui.utils', 'cfp.hotkeys'];
 
 	// Add a new vertical module
 	var registerModule = function(moduleName, dependencies) {
